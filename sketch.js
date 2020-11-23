f984fa0705fada22d4e8a5175a9e07f906b9b426
Kevin <rabinowitzkevin@gmail.com>
2020-11-23 03:28:40 -0500

    init commit

---
+++ sketch.js
@@ -0,0 +1,79 @@
+const video = document.getElementById("myvideo");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+let trackButton = document.getElementById("trackbutton");
+let updateNote = document.getElementById("updatenote");
+
+let isVideo = false;
+let model = null;
+let vid;
+
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video  
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.6,    // confidence threshold for predictions.
+}
+
+function startVideo() {
+    handTrack.startVideo(video).then(function (status) {
+        console.log("video started", status);
+        if (status) {
+            updateNote.innerText = "Video started. Now tracking"
+            isVideo = true
+            runDetection()
+        } else {
+            updateNote.innerText = "Please enable video"
+        }
+    });
+}
+
+function toggleVideo() {
+    if (!isVideo) {
+        updateNote.innerText = "Starting video"
+        startVideo();
+    } else {
+        updateNote.innerText = "Stopping video"
+        handTrack.stopVideo(video)
+        isVideo = false;
+        updateNote.innerText = "Video stopped"
+    }
+}
+
+
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        console.log("Predictions: ", predictions);
+        model.renderPredictions([], canvas, context, video);
+        if (isVideo) {
+            requestAnimationFrame(runDetection);
+        }
+
+        if (predictions.length > 0) {
+            sendMidpointLocation(predictions[0].bbox)            
+          }
+    });
+}
+
+function sendMidpointLocation(value) {
+    let midValX = value[0] + value[2] / 2;
+    let midValY = value[1] + value[3] / 2;
+    graphics.liquifyHelper(midValX, midValY);
+}
+
+// Load the model.
+handTrack.load(modelParams).then(lmodel => {
+    // detect objects in the image.
+    model = lmodel
+    updateNote.innerText = "Loaded Model!"
+    trackButton.disabled = false
+});
+
+
+function setup() {
+}
+
+function draw() {
+  background(220);
+}
